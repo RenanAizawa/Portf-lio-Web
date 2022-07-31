df40f40a206af30caf381b4689afa712d55d1cd0
Renan Aizawa Alves Mascarenhas <renanaizawa@outlook.com>
2022-07-31 13:25:30 -0400

    configuração: restabilização de pasta public com arquivo index.html

--- renan-aizawa/src/App.js
+++ renan-aizawa/src/App.js
@@ -1,8 +1,15 @@
+import { Route, Switch } from 'react-router-dom';
+import Header from './componets/Header.jsx';
+import Home from './pages/Home.jsx';
+
 function App() {
   return (
-   <div>
-      <h1>Portifolio Web 2.0</h1>
-   </div>
+    <div>
+      <Header/>
+      <Switch>
+        <Route path="/" exact component={ Home }/>
+      </Switch>
+    </div>
   );
 }
 
